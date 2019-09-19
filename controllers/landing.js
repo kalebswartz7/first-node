6ebabb3e9e8ee7bdf7fc099ece0899ca32090661
kalebswartz7 <kalebswartz7@gmail.com>
2019-09-19 15:16:26 -0400

    Can now edit leads

--- controllers/landing.js
+++ controllers/landing.js
@@ -17,3 +17,36 @@ exports.show_leads = function(req, res, next) {
         res.render('landing', { title: 'Express', leads: leads }); //gets from views directory 
     })
 }
+
+exports.show_lead = function(req, res, next) {
+    return models.Lead.findOne( {
+        where : {
+            id : req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead', {lead : lead});
+    });
+}
+
+exports.show_edit_lead = function(req, res, next) {
+    return models.Lead.findOne( {
+        where : {
+            id : req.params.lead_id
+        }
+    }).then(lead => {
+        res.render('lead/edit_lead', {lead : lead});
+    });
+}
+
+exports.edit_lead = function(req, res, next) {
+    return models.Lead.update({
+        email : req.body.lead_email
+    }, {
+        where: {
+            id: req.params.lead_id
+        }
+    }).then(result => {
+        res.redirect('/lead/' + req.params.lead_id);
+    })
+}
+
