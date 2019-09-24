559d683a57e23d80fcdc04d0402543d2cea1eff2
kalebswartz7 <kalebswartz7@gmail.com>
2019-09-24 15:40:22 -0400

    Revert "New packages for validation added"
    This reverts commit 90ebfeb3534e35d1e20dda45d570e214d9757fc7.

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,6 @@ let user = require('../controllers/user');
 
 router.get('/login', user.show_login);
 router.get('/signup', user.show_signup);
-router.post('/login', user.login);
-router.post('/signup', user.signup);
 /* GET home page. */
 router.get('/', landing.get_landing);
 router.post('/', landing.submit_lead);
